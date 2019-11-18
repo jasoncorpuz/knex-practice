2aab33a281a849a5514f58e269dff88aba9d9301
Jason Corpuz <jason.corpuz@live.com>
2019-11-18 12:20:02 -0800

    init commit

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+const app = require('../src/app')
+
+//use npm t-- --watch to test
+
+describe('App', () => {
+  it('GET / responds with 200 containing "Hello, world!"', () => {
+    return supertest(app)
+      .get('/')
+      .expect(200, 'Hello, world!')
+  })
+})
